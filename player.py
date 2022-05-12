c28aa7d10a95827ad39a767a4b34764dbbb71df9
MRWaelGhandoura <waelghandouranew@gmail.com>
2022-05-12 01:07:57 +0000

    fixing errors

--- player.py
+++ player.py
@@ -4,35 +4,34 @@ import random
 
 class Player():
     """
-    A superclass is created for the user, the computer  level
-    and computer hard level.
-    Player class to select a tag ( X or O) and a game.
-
+    A superclass is created for the user, the computer  level 
+    and computer hard level. 
+    Player class to select a inputTag  X  O and a game.
+    
     """
-    def __init__(self, letter):
-        self.letter = letter
+    def __init__(self, character):
+        self.character = character
 
     def get_move(self, game):
         pass
 
-
 class RealPlayer(Player):
-    """
+     """
      The RealPlayer class is used to define a real player
-     who plays against another real player.
-     It makes its moves taking into account the player's input
+     who plays against another real player. 
+     It makes its moves taking into account the player's input 
      regarding the available squares on the board.
 
      """
 
-    def __init__(self, letter):
-        super().__init__(letter)
+    def __init__(self, character):
+        super().__init__(character)
 
     def get_move(self, game):
         valid_square = False
         val = None
         while not valid_square:
-            square = input(self.letter + '\'s turn. Input move (0-9): ')
+            square = input(self.character + '\'s turn. Input move (0-8): ')
             try:
                 val = int(square)
                 if val not in game.available_moves():
@@ -42,18 +41,16 @@ class RealPlayer(Player):
                 print('Invalid move. Try again.')
         return val
 
-
 class NoobComputerPlayer(Player):
     """
     The class noobComputerPlayer is used to define a computer
-    against which the user can play.
-    It makes its moves randomly, depending on what moves
-    are available on the game board.
+    against which the user can play. 
+    It makes its moves randomly, depending on what moves are available on the game board.
 
     """
 
-    def __init__(self, letter):
-        super().__init__(letter)
+    def __init__(self, character):
+        super().__init__(character)
 
     def get_move(self, game):
         square = random.choice(game.available_moves())
@@ -65,21 +62,21 @@ class ProComputerPlayer(Player):
     ProComputerPlayer is a class that defines an unbeatable
     computer player that uses a minimax algorithm to function.
     The algorithm maximizes his score while minimizing his
-    losses, making it impossible to defeat him.
+     losses, making it impossible to defeat him.
 
     """
-    def __init__(self, letter):
-        super().__init__(letter)
+    def __init__(self, input_tag):
+        super().__init__(input_tag)
 
     def get_move(self, game):
         if len(game.available_moves()) == 9:
             square = random.choice(game.available_moves())
         else:
-            square = self.minimax(game, self.letter)['position']
+            square = self.minimax(game, self.input_tag)['position']
         return square
 
     def minimax(self, state, player):
-        max_player = self.letter  # User
+        max_player = self.input_tag  # User
         other_player = 'O' if player == 'X' else 'X'
 
         # Checking the previous move if it is a win move
@@ -108,5 +105,4 @@ class ProComputerPlayer(Player):
             else:
                 if sim_score['score'] < best['score']:
                     best = sim_score
-
-        return best
+        return best        
--- run.py
+++ run.py
@@ -1,162 +1,76 @@
-import math
-import time
 from player import RealPlayer, NoobComputerPlayer, ProComputerPlayer
 from termcolor import colored
+import time
 import pyfiglet
 
 
-ASCII_BANNER = pyfiglet.figlet_format(
-    "Hello!! \n WELCOME TO TIC TAC TOE GAME!!")
-COLOR_CHOICES = ["1- Red", "2-Yellow"]
-
-
 # A function to print the Tic Tac Toe board
-class Game:
+class TicTacToe:
     """
     A class to define the structure of the Tic tac toe game:
     A welcome massage
     A Game board
     A Display board
-    To Make a move with a tag
+    To Make a move with a input tag
     To Validate the move
     To check if the last move is a winning move
 
     """
-    def __init__(self, username, game_mode):
-        self.username = username
-        self.color = self.get_color()
-        self.board = [' ' for _ in range(9)]  # 3x3 game board
-        self.current_winner = None  # track the winner
-        if game_mode == 'easy':
-            print(f"You've selected {game_mode}, good luck!\n")
-            self.computer = NoobComputerPlayer('O')
-        elif game_mode == 'hard':
-            print(f"You've selected {game_mode}, try your hardest to beat the computer!\n")  # noqa
-            self.computer = ProComputerPlayer('O')
-        self.player = RealPlayer('X')
+    def __init__(self):
+        self.board = self.make_board()
+        self.current_winner = None
 
+    @staticmethod
+    def make_board():
+        return [' ' for _ in range(9)]
 
     def print_board(self):
-        print('')
-        print("Available Moves       Game Board")
-        for num, row in enumerate([self.board[i*3:(i+1)*3] for i in range(3)]):
-            availableMoves = self.available_moves_display()
-            availableRow = availableMoves[num*3:(num+1)*3]
-            availablePart = '| ' + ' | '.join(availableRow) + ' |'
-            boardPart = '       | ' + ' | '.join(row) + ' |'
-            print(availablePart + boardPart)
+        for row in [self.board[i*3:(i+1) * 3] for i in range(3)]:
+            print('| ' + ' | '.join(row) + ' |')
 
     @staticmethod
     def print_board_nums():
-        """
-        show us the number corresponds to each empty space
-        1 | 2 | 3 etc.
-        """
-        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]  # noqa
+        # 0 | 1 | 2
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
         for row in number_board:
-            numered_board = '| ' + ' | '.join(row) + ' |'
-            print(numered_board)
+            print('| ' + ' | '.join(row) + ' |')
 
-        print('')
-
-    def play(game_mode, print_game=True):
-    """
-    returns the winner of the game or None if it's a tie
-    """
-
-    game = Game(username, game_mode)
-
-    if print_game:
-        game.print_board_nums()
-
-    tag = 'X'  # starting tag
-
-    while game.empty_squares():
-        """
-        iterate as long as the game-board still has empty squares
-        gets move from appropriate player
-        """
-        if tag == 'O':
-            square = game.computer.get_move(game)
-        else:
-            square = game.player.get_move(game)
-
-        if game.make_move(square, tag):
-            if print_game:
-                print(tag + f' makes a move to square {square}')
-                game.print_board()
-                print('')  # empty line
-
-            if game.current_winner:
-                if print_game:
-                    print(tag + ' wins!')
-                return tag
-
-            # alternates / switches players for each round
-            if tag == 'X':
-                tag = 'O'
-            else:
-                tag = 'X'
-
-        time.sleep(2)  # a break in between moves to make the game more user friendly # noqa
-    if print_game:
-        print('It\'s a tie!') 
-
-    def available_moves_display(self):
-        moves = []
-        for (i, spot) in enumerate(self.board):
-            if spot == ' ':
-                moves.append(str(i))
-            else:
-                moves.append(' ')
-        return moves
-
-    def available_moves(self):
-        moves = []
-        for (i, spot) in enumerate(self.board):
-            if spot == ' ':
-                moves.append(i)
-        return moves
-
-    def empty_squares(self):
-        return ' ' in self.board
-
-    def num_empty_squares(self):
-        return self.board.count(' ')
-
-    def make_move(self, square, tag):
-        """
-        if the move is valid, proceed & return True
-        if the move is invalid, return False
-        """
+    def make_move(self, square, character):
         if self.board[square] == ' ':
-            self.board[square] = tag
-            if self.winner(square, tag):
-                self.current_winner = tag
+            self.board[square] = character
+            if self.winner(square, character):   # check the winner
+                self.current_winner = character
             return True
-        return False       
+        return False
 
-    def winner(self, square, letter):
-        # check the row
-        row_ind = math.floor(square / 3)
-        row = self.board[row_ind*3:(row_ind+1)*3]
-        # print('row', row)
-        if all([s == letter for s in row]):
+    # code credit: https://www.youtube.com/watch?v=n2o8ckO-lfk&list=PLAA3uifegEK98UkLE614yowsG7OHfANz9&index=1&t=663s
+    def winner(self, square, character):
+        """
+        winner if player ticks 3 in a row (either vertically, horizontally, or diagonally) # noqa
+        """
+
+        # checks row
+        row_ind = square // 3
+        row = self.board[row_ind*3: (row_ind + 1) * 3]
+        if all([spot == character for spot in row]):
             return True
+
+        # checks column
         col_ind = square % 3
         column = [self.board[col_ind+i*3] for i in range(3)]
-        # print('col', column)
-        if all([s == letter for s in column]):
+        if all([spot == character for spot in column]):
             return True
+
+        # checks diagonally
+        # only possible if all squares are even numbers
         if square % 2 == 0:
             diagonal1 = [self.board[i] for i in [0, 4, 8]]
-            # print('diag1', diagonal1)
-            if all([s == letter for s in diagonal1]):
+            if all([spot == character for spot in diagonal1]):
                 return True
             diagonal2 = [self.board[i] for i in [2, 4, 6]]
-            # print('diag2', diagonal2)
-            if all([s == letter for s in diagonal2]):
+            if all([spot == character for spot in diagonal2]):
                 return True
+
         return False
 
     def empty_squares(self):
@@ -167,35 +81,46 @@ class Game:
 
     def available_moves(self):
         return [i for i, x in enumerate(self.board) if x == " "]
-        
 
-    def get_color(self):
-        print("choose your color")
-        print(COLOR_CHOICES)
-        color = input().strip()
-        while color not in ["1", "2"]:
-            print("you must enter 1 or 2")
-            color = input().strip()
-        if color == "1":
-            self.color = 'red'
-        elif color == "2":
-            self.color = 'yellow'
-        print(colored("You choose your color !!", self.color))
-        print(color)
+    def play(game, x_player, o_player, print_game=True):
+        """
+        returns the winner of the game or None if it's a tie
+        """
 
+    if print_game:
+        game.print_board_nums()
 
+    character = 'X'
+    while game.empty_squares():
+        if character == 'O':
+            square = o_player.get_move(game)
+        else:
+            square = x_player.get_move(game)
+        if game.make_move(square, character):
 
-def welcome():
-    print(ASCII_BANNER)
-    print("Please Enter your name: \n")
-    username = input().strip()
+            if print_game:
+                print(character + ' makes a move to square {}'.format(square))
+                game.print_board()
+                print('')
 
-    while len(username) == 0:
-        print("It looks like you haven't typed anything, please enter your name!")  # noqa
-        username = input().strip()
-    # return hello message
-    print("\nHi " + f"{username}!" + " Nice to meet you!\n")
-    game = Game(username)
+            if game.current_winner:
+                if print_game:
+                    print(character + ' wins!')
+                return character  # ends loop and exits the game
+            
+            # alternates between players each round
+            if character == 'X':
+                character = 'O'
+            else:
+                character = 'X'
 
+        time.sleep(.8)
 
-welcome()
+    if print_game:
+        print('It\'s a tie!')
+
+if __name__ == '__main__':
+    x_player = ProComputerPlayer('X')
+    o_player = RealPlayer('O')
+    t = TicTacToe()
+    play(t, x_player, o_player, print_game=True)
